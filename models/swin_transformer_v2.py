68494feb91fedbdbe16e40a17da5046de22cd287
Caitlin Haddow <caitlinhaddow@gmail.com>
2025-08-28 20:11:50 +0100

    Code cleaned and commented for submission

--- models/swin_transformer_v2.py
+++ models/swin_transformer_v2.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.utils.checkpoint as checkpoint
 # from timm.models.layers import DropPath, to_2tuple, trunc_normal_
-from timm.layers import DropPath, to_2tuple, trunc_normal_ ## ch change
+from timm.layers import DropPath, to_2tuple, trunc_normal_  ## CH Dissertation: Update to meet package version requirements
 import numpy as np
 
 
